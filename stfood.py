c5f9ee8ffafeb3d23a82e0c11919e5704562e20d
monkas24x7 <shubhanjan.dash@gmail.com>
2021-05-24 22:49:17 +0530

    error in code repair

---
+++ stfood.py
@@ -0,0 +1,13 @@
+def check(s,p,v):
+	if p%(s+1)==0:
+		return (p//(s+1))*v
+	else:
+		return p//s+1
+t=int(input())
+for x in range(t):
+	n=int(input())
+	best=0
+	for y in range(n):	
+		s,p,v=[int (x) for x in input().rstrip().split()]
+		if check(s,p,v)>best: best=check(s,p,v)
+	print(best)
