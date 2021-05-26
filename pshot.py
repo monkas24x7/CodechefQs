b0696b23328c106d60935fb2291c8613dd5fb296
monkas24x7 <shubhanjan.dash@gmail.com>
2021-05-26 23:40:34 +0530

    still broken penalty problem

--- pshot.py
+++ pshot.py
@@ -1,13 +1,8 @@
 def check(arr):
-	a=0
-	b=0
+	size=len(arr)//2
 	for x in range(len(arr)):
-		if x%2==0:
-			a+=arr[x]
-		else:
-			b+=arr[x]
-			if a==len(arr)//2-1 and b<=(x+1)//2:
-				return x+1
+		if sum(arr[0:x+1:2])+size-(x+1)//2<sum(arr[1:x+1:2]) or sum(arr[1:x+1:2])+size-(x+1)//2<sum(arr[0:x+1:2]):
+			return x+1
 	return x+1
 t=int(input())
 for x in range(t):
