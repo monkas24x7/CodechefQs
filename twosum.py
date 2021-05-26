735d7c2b1df99c55e5bc295740ee251d08b83ea6
monkas24x7 <shubhanjan.dash@gmail.com>
2021-05-27 00:26:21 +0530

    bruteforce, try hashtable imp

--- twosum.py
+++ twosum.py
@@ -1,4 +1,9 @@
+nums=[2,7,11,15]
+target=9
 dic={}
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-	for x in nums:
-		if str(target-x) in  
+def twoSum(nums, target):
+	for x in range(len(nums)):
+		if str(target-nums[x]) in  dic.keys():
+			print ([dic[str(target-nums[x])],x])
+		dic[str(nums[x])]=x 
+twoSum(nums,target) 
