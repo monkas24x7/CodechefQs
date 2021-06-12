dae0d7d869973970fb50337ae0065d4ca7811843
monkas24x7 <shubhanjan.dash@gmail.com>
2021-06-12 13:47:53 +0530

    completed chfheist

--- chfheist.py
+++ chfheist.py
@@ -1,7 +1,7 @@
 def check(D,d,p,q):
 	n=D//d
 	rem=D%d
-	total=(((n*p+(((n**2-n)*q)/2))*d)+(rem*(p+(n*q))))
+	total=(((n*p+(((n**2-n)*q)//2))*d)+(rem*(p+(n*q))))
 	return total 
 n=int(input())
 for x in range(n):
