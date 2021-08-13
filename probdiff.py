2f3fd7db7c7fb76cfe9cf36d5de0f3e25c363645
monkas24x7 <shubhanjan.dash@gmail.com>
2021-08-13 22:02:17 +0530

    solved some qs

--- probdiff.py
+++ probdiff.py
@@ -1,14 +1,11 @@
+def calc(a):
+	aa=set(a)
+	if len(aa)>=3: return 2
+	elif len(aa)==2:
+		if a.count(a[0])==2:
+			return 2
+		else: return 1
+	else: return 0
 n=int(input())
 for x in range(n):
-	n,p,k=[int(x) for x in input().rstrip().split()]
-	s=1
-	c,cc,m=0,0,0
-	while(s!=p):
-		s=cc*k+m
-		if s>n: 
-			m+=1
-			cc=0
-			continue
-		cc+=1
-		c+=1
-	print(c-1)
+	print(calc(input().rstrip().split()))
