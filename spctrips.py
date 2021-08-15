295d16a510bb47058504ab813cb2fb26de68a984
monkas24x7 <shubhanjan.dash@gmail.com>
2021-08-15 19:13:59 +0530

    solved some qs

--- spctrips.py
+++ spctrips.py
@@ -1,1 +1,16 @@
-ECHO is on.
+def factors(x):
+	c=[1]
+	for i in range(2,x//2+1):
+		if x%i==0:
+			c.append(i)
+	return c
+def calc(num):
+	total=0
+	for x in range(2,num+1):
+		num//x
+	return total
+n=int(input())
+for i in range(n):
+	num=int(input())
+	print(calc(num))
+print(factors(10**9))
--- twosum.py
+++ twosum.py
@@ -1,9 +1,1 @@
-nums=[2,7,11,15]
-target=9
-dic={}
-def twoSum(nums, target):
-	for x in range(len(nums)):
-		if str(target-nums[x]) in  dic.keys():
-			print ([dic[str(target-nums[x])],x])
-		dic[str(nums[x])]=x 
-twoSum(nums,target) 
+ECHO is on.
